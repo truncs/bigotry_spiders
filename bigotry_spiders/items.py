4901853e8d6d99b43f57e18f130968810a6a8478
truncs <sarawgi.aditya@gmail.com>
2011-11-06 14:27:25 -0500

    Write some sample code

--- bigotry_spiders/items.py
+++ bigotry_spiders/items.py
@@ -6,6 +6,6 @@
 from scrapy.item import Item, Field
 
 class BigotrySpidersItem(Item):
-    # define the fields for your item here like:
-    # name = Field()
-    pass
+    title = Field()
+    link = Field()
+    desc = Field()
